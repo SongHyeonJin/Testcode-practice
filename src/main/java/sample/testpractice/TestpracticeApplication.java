12edbb95d01853cae7996f5b19f4735f340fb4a5
HyeonJin Song <shwj1602@naver.com>
2023-10-22 21:47:05 +0900

    init : project settings

---
+++ src/main/java/sample/testpractice/TestpracticeApplication.java
@@ -0,0 +1,13 @@
+package sample.testpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestpracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sample/testpractice/TestpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.testpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
