7d696b470e5b116a31ac87e1912d9b70d05410e3
HyeonJin Song <shwj1602@naver.com>
2023-11-03 22:13:53 +0900

    Test : DisplayName 추가

--- src/test/java/sample/testpractice/unit/CafeKioskTest.java
+++ src/test/java/sample/testpractice/unit/CafeKioskTest.java
@@ -1,5 +1,6 @@
 package sample.testpractice.unit;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import sample.testpractice.unit.beverage.Americano;
 import sample.testpractice.unit.beverage.Latte;
@@ -22,6 +23,7 @@ class CafeKioskTest {
         System.out.println(">>> 담긴 음료 : " + cafeKiosk.getBeverages().get(0).getName());
     }
 
+    @DisplayName("음료 1개 추가하면 주문 목록에 담긴다.")
     @Test
     void add(){
         CafeKiosk cafeKiosk = new CafeKiosk();
@@ -78,8 +80,10 @@ class CafeKioskTest {
         assertThat(cafeKiosk.getBeverages()).isEmpty();
     }
 
+    @DisplayName("주문 목록에 담긴 상품들의 총 금액을 계산할 수 있다.")
     @Test
     void calculateTotalPrice(){
+        // given
         CafeKiosk cafeKiosk = new CafeKiosk();
         Americano americano = new Americano();
         Latte latte = new Latte();
@@ -87,8 +91,10 @@ class CafeKioskTest {
         cafeKiosk.add(americano);
         cafeKiosk.add(latte);
 
+        // when
         int totalPrice = cafeKiosk.calculateTotalPrice();
 
+        // then
         assertThat(totalPrice).isEqualTo(8500);
     }
 
