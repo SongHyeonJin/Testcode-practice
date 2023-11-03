46c22219eba3e250d261840245b1efdce92286b7
HyeonJin Song <shwj1602@naver.com>
2023-11-03 16:14:59 +0900

    Chore : 전체가격 로직 수정

--- src/main/java/sample/testpractice/unit/CafeKiosk.java
+++ src/main/java/sample/testpractice/unit/CafeKiosk.java
@@ -42,11 +42,9 @@ public class CafeKiosk {
     }
 
     public int calculateTotalPrice() {
-        int totalPrice = 0;
-        for (Beverage beverage : beverages) {
-            totalPrice += beverage.getPrice();
-        }
-        return totalPrice;
+        return beverages.stream()
+                .mapToInt(Beverage::getPrice)
+                .sum();
     }
 
     public Order createOrder(){
@@ -67,4 +65,5 @@ public class CafeKiosk {
 
         return new Order(currentDateTime, beverages);
     }
+
 }
