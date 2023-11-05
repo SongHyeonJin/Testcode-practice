df886182dd5dc2cf90a62a7301623f12b6aecede
HyeonJin Song <shwj1602@naver.com>
2023-11-06 04:09:09 +0900

    Chore : 중복되는 상품 추가 정상 작동되게 로직 수정

--- src/main/java/sample/testpractice/spring/api/service/order/OrderSerivce.java
+++ src/main/java/sample/testpractice/spring/api/service/order/OrderSerivce.java
@@ -11,6 +11,8 @@ import sample.testpractice.spring.domain.product.ProductRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -22,9 +24,15 @@ public class OrderSerivce {
     public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredDateTime) {
         List<String> productNumbers = request.getProductNumbers();
         List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
+        Map<String, Product> productMap = products.stream()
+                .collect(Collectors.toMap(Product::getProductNumber, p -> p));
+
+        List<Product> duplicationProducts = productNumbers.stream()
+                .map(productMap::get)
+                .collect(Collectors.toList());
 
         // Order
-        Order order = Order.create(products, registeredDateTime);
+        Order order = Order.create(duplicationProducts, registeredDateTime);
         Order saveOrder = orderRepository.save(order);
         return OrderResponse.of(saveOrder);
     }
