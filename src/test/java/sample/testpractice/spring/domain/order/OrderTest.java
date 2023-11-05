f7482a64255a1f05bff561530a49122157353a16
HyeonJin Song <shwj1602@naver.com>
2023-11-05 23:10:44 +0900

    Test : 전체 금액 테스트와 주문 생성 시 상태 체크 테스트

---
+++ src/test/java/sample/testpractice/spring/domain/order/OrderTest.java
@@ -0,0 +1,58 @@
+package sample.testpractice.spring.domain.order;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import sample.testpractice.spring.domain.product.Product;
+import sample.testpractice.spring.domain.product.ProductSellingStatus;
+import sample.testpractice.spring.domain.product.ProductType;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderTest {
+
+    @DisplayName("주문 생성 시 상품 리스트에서 주문의 총 금액을 계산한다.")
+    @Test
+    void calculateTotalPrice(){
+        // given
+        List<Product> products = List.of(
+                createProduct("001", 1000),
+                createProduct("002", 2000)
+        );
+
+        // when
+        Order order = Order.create(products);
+
+        // then
+        assertThat(order.getTotalPrice()).isEqualTo(3000);
+     }
+
+    @DisplayName("주문 생성 시 주문 상태는 INIT이다.")
+    @Test
+    void init(){
+        // given
+        List<Product> products = List.of(
+                createProduct("001", 1000),
+                createProduct("002", 2000)
+        );
+
+        // when
+        Order order = Order.create(products);
+
+        // then
+        assertThat(order.getOrderStatus()).isEqualByComparingTo(OrderStatus.INIT);
+    }
+
+    private Product createProduct(String productNumber, int price){
+        return Product.builder()
+                .type(ProductType.HANDMADE)
+                .productNumber(productNumber)
+                .price(price)
+                .sellingStatus(ProductSellingStatus.SELLING)
+                .name("메뉴 이름")
+                .build();
+    }
+
+}
