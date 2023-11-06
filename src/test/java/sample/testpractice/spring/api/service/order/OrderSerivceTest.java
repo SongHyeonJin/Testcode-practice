bbf974cbcf4507c740e26fbc9aa9f62f61ce2cfb
HyeonJin Song <shwj1602@naver.com>
2023-11-07 00:07:55 +0900

    Test : 재고와 관련된 상품이 포함되어 있는 주문 번호 리스트 받아 주문 생성 테스트

--- src/test/java/sample/testpractice/spring/api/service/order/OrderSerivceTest.java
+++ src/test/java/sample/testpractice/spring/api/service/order/OrderSerivceTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 import sample.testpractice.spring.api.controller.order.request.OrderCreateRequest;
 import sample.testpractice.spring.api.service.order.response.OrderResponse;
 import sample.testpractice.spring.domain.order.OrderRepository;
@@ -15,6 +16,8 @@ import sample.testpractice.spring.domain.product.Product;
 import sample.testpractice.spring.domain.product.ProductRepository;
 import sample.testpractice.spring.domain.product.ProductSellingStatus;
 import sample.testpractice.spring.domain.product.ProductType;
+import sample.testpractice.spring.domain.stock.Stock;
+import sample.testpractice.spring.domain.stock.StockRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -24,8 +27,8 @@ import static org.assertj.core.api.Assertions.tuple;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ActiveProfiles("test")
+@Transactional
 @SpringBootTest
-//@DataJpaTest
 class OrderSerivceTest {
 
     @Autowired
@@ -35,14 +38,16 @@ class OrderSerivceTest {
     @Autowired
     private OrderProductRepository orderProductRepository;
     @Autowired
+    private StockRepository stockRepository;
+    @Autowired
     private OrderSerivce orderSerivce;
 
-    @AfterEach
-    void tearDown() {
-        orderProductRepository.deleteAllInBatch();
-        productRepository.deleteAllInBatch();
-        orderRepository.deleteAllInBatch();
-    }
+//    @AfterEach
+//    void tearDown() {
+//        orderProductRepository.deleteAllInBatch();
+//        productRepository.deleteAllInBatch();
+//        orderRepository.deleteAllInBatch();
+//    }
 
     @DisplayName("주문번호 리스트를 받아 주문을 생성한다.")
     @Test
@@ -74,6 +79,51 @@ class OrderSerivceTest {
                         tuple("002", 3000)
                 );
      }
+
+    @DisplayName("재고와 관련된 상품이 포함되어 있는 주문번호 리스트를 받아 주문을 생성한다.")
+    @Test
+    void createOrderWithStock(){
+        // given
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
+        Product product1 = createProduct(ProductType.BOTTLE, "001", 1000);
+        Product product2 = createProduct(ProductType.BAKERY, "002", 3000);
+        Product product3 = createProduct(ProductType.HANDMADE, "003", 5000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        Stock stock1 = Stock.create("001", 2);
+        Stock stock2 = Stock.create("002", 2);
+        stockRepository.saveAll(List.of(stock1, stock2));
+
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of("001", "001", "002", "003"))
+                .build();
+
+        // when
+        OrderResponse orderResponse = orderSerivce.createOrder(request, registeredDateTime);
+
+        // then
+        assertThat(orderResponse.getId()).isNotNull();
+        assertThat(orderResponse)
+                .extracting("registeredDateTime", "totalPrice")
+                .contains(registeredDateTime, 10000);
+        assertThat(orderResponse.getProducts()).hasSize(4)
+                .extracting("productNumber", "price")
+                .containsExactlyInAnyOrder(
+                        tuple("001", 1000),
+                        tuple("001", 1000),
+                        tuple("002", 3000),
+                        tuple("003", 5000)
+                );
+
+        List<Stock> stocks = stockRepository.findAll();
+        assertThat(stocks).hasSize(2)
+                .extracting("productNumber", "quantity")
+                .containsExactlyInAnyOrder(
+                        tuple("001", 0),
+                        tuple("002", 1)
+                );
+    }
 
     @DisplayName("중복되는 상품번호 리스트로 주문을 생성할 수 있다.")
     @Test
