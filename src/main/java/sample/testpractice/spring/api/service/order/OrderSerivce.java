a68cd4b1ea03fdd0c1ba300fc51b062abf0d636f
HyeonJin Song <shwj1602@naver.com>
2023-11-07 00:07:20 +0900

    Feat : 재고 차감 체크 로직, 재고 조회 로직, 상품별 카운팅 로직, 재고 차감시도 로직 추가

--- src/main/java/sample/testpractice/spring/api/service/order/OrderSerivce.java
+++ src/main/java/sample/testpractice/spring/api/service/order/OrderSerivce.java
@@ -8,6 +8,9 @@ import sample.testpractice.spring.domain.order.Order;
 import sample.testpractice.spring.domain.order.OrderRepository;
 import sample.testpractice.spring.domain.product.Product;
 import sample.testpractice.spring.domain.product.ProductRepository;
+import sample.testpractice.spring.domain.product.ProductType;
+import sample.testpractice.spring.domain.stock.Stock;
+import sample.testpractice.spring.domain.stock.StockRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -20,12 +23,37 @@ public class OrderSerivce {
 
     private final ProductRepository productRepository;
     private final OrderRepository orderRepository;
+    private final StockRepository stockRepository;
 
     public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredDateTime) {
         List<String> productNumbers = request.getProductNumbers();
         List<Product> products = findPProductsBy(productNumbers);
 
-        // Order
+        // 재고 차감 체크가 필요한 상품들 filter
+        List<String> stockProductNumbers = products.stream()
+                .filter(product -> ProductType.containsStockType(product.getType()))
+                .map(Product::getProductNumber)
+                .collect(Collectors.toList());
+
+        // 재고 엔티티 조회
+        List<Stock> stocks = stockRepository.findAllByProductNumberIn(stockProductNumbers);
+        Map<String, Stock> stockMap = stocks.stream()
+                .collect(Collectors.toMap(Stock::getProductNumber, s -> s));
+
+        // 상품별 counting
+        Map<String, Long> productCountingMap = stockProductNumbers.stream()
+                .collect(Collectors.groupingBy(p -> p, Collectors.counting()));
+
+        // 재고 차감 시도
+        for (String stockProductNumber : stockProductNumbers) {
+            Stock stock = stockMap.get(stockProductNumber);
+            int quantity = productCountingMap.get(stockProductNumber).intValue();
+            if(stock.isQuantityLessThan(quantity)){
+                throw new IllegalArgumentException("재고가 부족한 상품이 있습니다.");
+            }
+            stock.deductQuantity(quantity);
+        }
+
         Order order = Order.create(products, registeredDateTime);
         Order saveOrder = orderRepository.save(order);
         return OrderResponse.of(saveOrder);
