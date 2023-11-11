1120b05bd365a573fd89e0406a79a13e237a1134
HyeonJin Song <shwj1602@naver.com>
2023-11-11 22:10:55 +0900

    Feat : 생성자 빌더 추가

--- src/main/java/sample/testpractice/spring/api/controller/product/dto/request/ProductCreateRequest.java
+++ src/main/java/sample/testpractice/spring/api/controller/product/dto/request/ProductCreateRequest.java
@@ -1,5 +1,6 @@
 package sample.testpractice.spring.api.controller.product.dto.request;
 
+import lombok.Builder;
 import lombok.Getter;
 import sample.testpractice.spring.domain.product.ProductSellingStatus;
 import sample.testpractice.spring.domain.product.ProductType;
@@ -11,5 +12,13 @@ public class ProductCreateRequest {
     private ProductSellingStatus sellingStatus;
     private String name;
     private int price;
+
+    @Builder
+    public ProductCreateRequest(ProductType type, ProductSellingStatus sellingStatus, String name, int price) {
+        this.type = type;
+        this.sellingStatus = sellingStatus;
+        this.name = name;
+        this.price = price;
+    }
 
 }
