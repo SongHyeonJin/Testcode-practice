3e44add4acdf1312679c8c8e1f6c61f567bbc831
HyeonJin Song <shwj1602@naver.com>
2023-11-12 11:29:08 +0900

    Feat : 트랜잭션 추가, 상품등록 엔티티 저장

--- src/main/java/sample/testpractice/spring/api/service/product/ProductService.java
+++ src/main/java/sample/testpractice/spring/api/service/product/ProductService.java
@@ -2,6 +2,7 @@ package sample.testpractice.spring.api.service.product;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import sample.testpractice.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.testpractice.spring.api.service.product.response.ProductResponse;
 import sample.testpractice.spring.domain.product.Product;
@@ -12,24 +13,30 @@ import sample.testpractice.spring.domain.product.ProductType;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/*
+* readOnly = true : 읽기전용
+* CRUD 에서 CUD 동작 X / only Read
+* JPA : CUD 스냅샷 저장, 변경감지 X (성능 향상)
+*
+* CQRS - Command / Query
+ */
 @RequiredArgsConstructor
 @Service
 public class ProductService {
 
     private final ProductRepository productRepository;
 
+    @Transactional
     public ProductResponse createProduct(ProductCreateRequest request) {
-        String nexttProductNumber = createNextProductNumber();
+        String nextProductNumber = createNextProductNumber();
 
-        return ProductResponse.builder()
-                .productNumber(nexttProductNumber)
-                .type(request.getType())
-                .sellingStatus(request.getSellingStatus())
-                .name(request.getName())
-                .price(request.getPrice())
-                .build();
+        Product product = request.toEntity(nextProductNumber);
+        Product savedProduct = productRepository.save(product);
+
+        return ProductResponse.of(savedProduct);
     }
 
+    @Transactional(readOnly = true)
     public List<ProductResponse> getSellingProducts(){
         List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
 
