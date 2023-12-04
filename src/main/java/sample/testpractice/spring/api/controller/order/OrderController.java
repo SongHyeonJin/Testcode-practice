77de83a747379b150fa92938fb4f1c478de95f87
HyeonJin Song <shwj1602@naver.com>
2023-12-04 16:14:40 +0900

    Feat : ApiResponse 로 리턴 추가

--- src/main/java/sample/testpractice/spring/api/controller/order/OrderController.java
+++ src/main/java/sample/testpractice/spring/api/controller/order/OrderController.java
@@ -4,10 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import sample.testpractice.spring.api.ApiResponse;
 import sample.testpractice.spring.api.controller.order.request.OrderCreateRequest;
 import sample.testpractice.spring.api.service.order.OrderSerivce;
 import sample.testpractice.spring.api.service.order.response.OrderResponse;
 
+import javax.validation.Valid;
 import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
@@ -17,9 +19,9 @@ public class OrderController {
     private final OrderSerivce orderSerivce;
 
     @PostMapping("/api/v1/orders/new")
-    public OrderResponse createOrder(@RequestBody OrderCreateRequest request){
+    public ApiResponse<OrderResponse> createOrder(@Valid @RequestBody OrderCreateRequest request){
         LocalDateTime registerdDateTime = LocalDateTime.now();
-        return orderSerivce.createOrder(request, registerdDateTime);
+        return ApiResponse.ok(orderSerivce.createOrder(request.toServiceReqeust(), registerdDateTime));
     }
 
 
